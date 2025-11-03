0c58f08a1131945fb9f98b9733cf5283e39da6eb
Asad Tariq <asaddc123@outlook.com>
2025-11-03 12:15:10 +0000

    Completed Lab3 Task 1

---
+++ labs/Lab3-FSM/task1/lfsr.sv
@@ -0,0 +1,26 @@
+module lfsr(
+    input   logic       clk,
+    input   logic       rst,
+    input   logic       en,
+    output  logic [3:0] data_out
+);
+    
+    logic [3:0] sreg;
+
+    
+    
+    always_ff @(posedge clk, posedge rst) begin
+    // output is synchronous
+    if(rst) sreg <= 4'b0001;
+    else if (en) begin
+    sreg[0] <= sreg[2] ^ sreg[3];
+    sreg[1] <= sreg[0];
+    sreg[2] <= sreg[1];
+    sreg[3] <= sreg[2];
+    end
+
+    
+end
+    assign data_out = sreg;
+
+endmodule
---
+++ labs/Lab3-FSM/task1/lfsr_7.sv
@@ -0,0 +1,10 @@
+module lfsr_7 (
+    input   logic       clk,
+    input   logic       rst,
+    input   logic       en,
+    output  logic [6:0] data_out
+);
+
+    
+
+endmodule
